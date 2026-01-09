b4ecebb446e3d76421ff58c334d130b12f0cd478
comradeshr00m <typedfrog@proton.me>
2026-01-09 21:17:12 +0000

    chore: rm niri cache

--- system/nixos.nix
+++ system/nixos.nix
@@ -36,10 +36,10 @@
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   nix.settings.auto-optimise-store = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  nix.settings.substituters = ["https://cache.nixos.org" "https://niri.cachix.org"];
+  nix.settings.substituters = ["https://cache.nixos.org" ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+    # "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
   ];
   # console = {
   #   font = "Lat2-Terminus16";
